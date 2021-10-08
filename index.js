4d9e35858398e968fb0e1f7a960b39f0e6780472
bindi-v <bindi.vaghela@gmail.com>
2021-10-07 22:20:42 -0400

    add templates in index.js and node_module in gitignore

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const questions = ([{
     name: 'Title',
     message: 'What is the title of the Project?'
 }, {
-type: 'input',
-name: 'Description',
-message: 'What is the project about? Give a detailed description of your project.'
+    type: 'input',
+    name: 'Description',
+    message: 'What is the project about? Give a detailed description of your project.'
 }, {
-type: 'input',
-name: 'Table of Contents',
-message: 'Table of Contents'
+    type: 'input',
+    name: 'Table of Contents',
+    message: 'Table of Contents'
 },
 {
     type: 'input',
@@ -29,10 +29,14 @@ message: 'Table of Contents'
     name: 'Usage',
     message: 'How is the application used? Give instructions to use application.'
 }, {
-    type: 'input',
+    type: 'list',
     name: 'License',
     message: 'What license did you used for this application?',
     choices: ['MIT', 'GPL', 'Apache', 'GNU', 'N/A']
+},{
+   type: 'input',
+   name: 'Contributions',
+   message: 'Who contributes in this project?'
 }, {
     type: 'input',
     name: 'Github Username',
@@ -41,6 +45,55 @@ message: 'Table of Contents'
     type: 'input',
     name: 'Email',
     message: 'What is your email Id?'
+}, {
+    type: 'input',
+    name: 'Questions',
+    message: 'If you have any questions related to project? Reach out me at :'
 }
 ])
+.then(({
+    title,
+    description,
+    tableOfContent,
+    installation,
+    usage,
+    license,
+    contribution,
+    github,
+    email,
+    questions 
+})  => {
+    //using template
+    const template =`# ${title}
+    
+    # ${data.title}
+    # Description 
+    $ {data.Description}
+    # Table Of Contents
+
+    * [Installation](#installation)
+    * [Usage] (#usage)
+    * [License] (#license)
+    * [Contribution] (#contribution)
+    * [GitHub] (#github)
+    * [Email] (#email)
+    # Installation 
+    $ {installation}
+    # Usage
+    $ {usage}
+    # License
+    $ {license}
+    # Contribution
+    $ {contribution}
+    # Contact
+    * GitHub : $ {github}
+    * Email : $ {email}`
+    
+    createNewFile(title, template);
+});
+
+function createNewFile(newFileName, data) {
+fs.writeFile(${newFileName}, data)
+}
+
 
